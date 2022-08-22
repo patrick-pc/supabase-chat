aa65382d47853b1e31f25a435990b2f076be10bc
spidΞy <web3slinger@gmail.com>
2022-08-21 22:00:52 -0500

    Update styles

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <WagmiConfig client={client}>
       <ConnectKitProvider theme="soft">
-        <ConnectKitButton />
         <Component {...pageProps} />
         <Toaster />
       </ConnectKitProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { ConnectKitButton } from 'connectkit'
 import { useAccount, useEnsName } from 'wagmi'
 import { supabase } from '../utils/supabase'
@@ -8,6 +8,7 @@ import toast from 'react-hot-toast'
 export default function Home() {
   const [messages, setMessages] = useState([])
   const [message, setMessage] = useState('')
+  const inputRef = useRef(null)
 
   const { address } = useAccount()
   const { data: ensName } = useEnsName({
@@ -15,7 +16,6 @@ export default function Home() {
     chainId: 1,
   })
 
-  // Check wallet address
   useEffect(() => {
     console.log('address', address)
     console.log('ensName', ensName)
@@ -50,6 +50,10 @@ export default function Home() {
   }
 
   const sendMessage = async () => {
+    if (!message) {
+      return
+    }
+
     if (!address) {
       console.log('test')
       toast('Connect wallet to continue.', {
@@ -68,10 +72,7 @@ export default function Home() {
     ])
     console.log('insert')
 
-    if (error) {
-      console.log(error)
-      return
-    }
+    if (error) console.log(error)
   }
 
   return (
@@ -81,30 +82,40 @@ export default function Home() {
         <meta name="description" content="Supabase Chat" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex flex-col items-center justify-between h-[600px]">
-        <div className="flex flex-col-reverse overflow-y-auto">
-          {messages.map((message) => (
-            <div key={message.id}>
-              <p>{message.id}</p>
-              <p>{message.sender}</p>
-              <p>{message.content}</p>
-              <p>{message.timestamp}</p>
+
+      <div className="container mx-auto">
+        <div className="flex items-center justify-center text-sm mx-4 pt-40">
+          <div className="relative">
+            <div className="absolute -inset-0.5 bg-gradient-to-r from-red-500 to-lime-500 rounded-md blur opacity-75"></div>
+            <div
+              className="mockup-code w-full md:w-[600px] bg-black"
+              onClick={() => inputRef.current.focus()}
+            >
+              <div className="flex flex-col-reverse h-[300px] overflow-y-auto no-scrollbar">
+                {messages.map((message) => (
+                  <div key={message.id}>
+                    <pre data-prefix="$">
+                      <span className="text-zinc-400">{message.sender}:</span> {message.content}
+                    </pre>
+                    {/* <pre data-prefix="$">{message.timestamp}</pre> */}
+                  </div>
+                ))}
+              </div>
+              <pre data-prefix="$">
+                <input
+                  ref={inputRef}
+                  className="bg-transparent md:w-[530px] focus:outline-none"
+                  type="text"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  onKeyPress={(e) => {
+                    e.key === 'Enter' && sendMessage()
+                  }}
+                  maxLength={70}
+                />
+              </pre>
             </div>
-          ))}
-        </div>
-        <div>
-          <input
-            className="border"
-            type="text"
-            placeholder="Type your message here"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            onKeyPress={(e) => {
-              e.key === 'Enter' && sendMessage()
-            }}
-            maxLength={30}
-          />
-          <button onClick={sendMessage}>Send Message</button>
+          </div>
         </div>
       </div>
     </div>
